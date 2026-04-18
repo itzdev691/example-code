8870506eb10b0e77c50cc9215e7d91ac9d161c5c
itzdev691 <amordarsis@gmail.com>
2026-04-18 00:11:23 -0500

    Add cout.cpp test file to demonstrate std::cout usage in C++

---
+++ TESTS/cout.cpp
@@ -0,0 +1,11 @@
+// This file is a test file for demonstrating the use of std::cout in C++.
+
+// Find out what is wrong Phillip!
+
+include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    
+}
