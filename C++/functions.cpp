d2d83025a4f90502d7f4229857ab9a8d0bc004e2
itzdev691 <amordarsis@gmail.com>
2026-04-18 00:00:19 -0500

    Refactor code structure and improve documentation for clarity

---
+++ C++/functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int add() {
+    int a = 10;
+    int b = 20;
+    return a + b;
+   
+}
+
+int main() {
+    cout << add() << endl;
+    return 0;
+}
