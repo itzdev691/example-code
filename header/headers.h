7c7c27c83b2947f539162e7e425132dc3fb81f3f
itzdev691 <amordarsis@gmail.com>
2026-04-19 14:01:48 -0500

    Added a use of headers example

---
+++ header/headers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+// A .h file, or header file, is a text file used in C, C++, and Objective-C to store declarations of functions, classes, macros, and variables, allowing them to be shared across multiple source code
+
+int helloworld() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ header/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Here we are including the headers.h file, which contains the declaration of the helloworld function
+#include "headers.h"
+
+// Here we are calling the helloworld function
+int main() {
+    helloworld();
+    return 0;
+}
